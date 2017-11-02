2622505f3fa01e810a12aff9abcc47fe9b6a6663
Darshan Patel <darshanpatel25894@gmail.com>
2017-11-02 19:32:50 -0400

    (server.js) server handles new connects and disconnects

--- server.js
+++ server.js
@@ -11,3 +11,19 @@ console.log('Server running...');
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+io.sockets.on('connection', function(socket){
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  // Disconnect
+  socket.on('disconnect', function(data){
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  // Send Message
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data});
+  });
+});
